bce46a451ed6d47dde869dae9d098ab6f9c9d0a5
João Afonso Katombela <ja3328173@gmail.com>
2024-04-11 15:38:07 +0100

    index title

--- src/presentation/pages/contact/contact.tsx
+++ src/presentation/pages/contact/contact.tsx
@@ -15,7 +15,9 @@ import {
       <div className="container relative">
         <FloatingBalls />
         <NavBar /> 
-         
+         <div className="flex items-center justify-center py-16 border-b-2 border-primary ">
+            <h1 className="text-4xl font-bold text-white lg:text-5xl ">Get in Touch</h1>
+         </div>
         <Footer />
       </div>
     );
