ce911801dea60ff33e82d846bfe274fe51dc73e8
João Afonso Katombela <ja3328173@gmail.com>
2024-04-16 10:17:13 +0100

    card testimnonials

--- src/presentation/components/floating-balls/balls.tsx
+++ src/presentation/components/floating-balls/balls.tsx
@@ -1,9 +1,8 @@
-
-export function FloatingBalls  () {
+export function FloatingBalls() {
   return (
     <div className="z-0 balls">
-      <div className="violet h-[15rem] absolute rounded-full left-0 right-0 -top-20 opacity-[.1] mx-auto  w-[15rem] bg-violet"></div>
-      <div className="violet h-[10rem] absolute rounded-full left-[10%] top-20 opacity-[.1] mx-auto  w-[10rem] bg-primary"></div>
+      <div className="violet h-[15rem] absolute rounded-full left-0 right-0 -top-20 opacity-[.1] mx-auto  w-[15rem] bg-violet animate-float"></div>
+      <div className="violet h-[10rem] absolute rounded-full left-[10%] top-20 opacity-[.1] mx-auto  w-[10rem] bg-primary animate-float2"></div>
     </div>
   );
 }
