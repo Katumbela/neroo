d4b2215ffe4fb34083e98f839913171ef962ad70
João Afonso Katombela <ja3328173@gmail.com>
2024-04-17 10:56:52 +0100

    add vercel file and scroll up button

--- src/presentation/components/footer/footer.tsx
+++ src/presentation/components/footer/footer.tsx
@@ -108,7 +108,7 @@ export function Footer() {
       {showScrollButton && (
         <button
           onClick={handleScrollTop}
-          className="fixed flex items-center gap-2 px-4 py-2 text-center text-white duration-75 rounded-full shadow-lg bg-primary place-content-center bottom-4 right-4 animate-bounce"
+          className="fixed flex items-center gap-2 px-4 py-2 text-center text-white rounded-full shadow-lg click bg-primary place-content-center bottom-4 right-4 animate-bounce"
         >
           <FaArrowUp />
          <span className="hidden text-sm md:block"> Scroll To Top</span>
