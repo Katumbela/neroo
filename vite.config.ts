d18ad5dbb7b04cd0840efdde2d275150e2e8a9da
João Afonso Katombela <ja3328173@gmail.com>
2024-05-02 16:15:05 +0100

    fix : apply SEO

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+
+import { VitePWA } from 'vite-plugin-pwa'
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Neroo Marketing Agency",
+        short_name: "Neroo",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#000",
+        lang: "en",
+        scope: "/",
+        icons: [
+          {
+            src: "/src/assets/imgs/neroo (1).jpeg",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/src/assets/imgs/neroo (1).jpeg",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        theme_color: "#DD1102",
+      },
+    }),
+  ],
+});
